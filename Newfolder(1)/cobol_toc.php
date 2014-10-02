7ee3e8e3f009feff35dcb04676c2cd6d95ad4906
mcmohd <mcmohd@gmail.com>
2014-10-03 00:29:56 +0530

    New file created by the system

---
+++ Newfolder(1)/cobol_toc.php
@@ -0,0 +1,33 @@
+<div class="mini-logo">
+<img src="/cobol/images/cobol-mini-logo.png" alt="Cobol Tutorial" />
+</div>
+
+<ul class="nav nav-list primary left-menu">
+<li class="heading">Cobol Tutorial</li>
+<li><a href="/cobol/index.htm">Cobol - Home</a></li>
+<li><a href="/cobol/cobol_overview.htm">Cobol - Overview</a></li>
+<li><a href="/cobol/cobol_environment_setup.htm">Cobol - Environment Setup</a></li>
+<li><a href="/cobol/cobol_program_structure.htm">Cobol - Program Structure</a></li>
+<li><a href="/cobol/cobol_basic_syntax.htm">Cobol - Basic Syntax</a></li>
+<li><a href="/cobol/cobol_data_types.htm">Cobol - Data Types</a></li>
+<li><a href="/cobol/cobol_basic_verbs.htm">Cobol - Basic Verbs</a></li>
+<li><a href="/cobol/cobol_data_layout.htm">Cobol - Data Layout</a></li>
+<li><a href="/cobol/cobol_condition_statements.htm">Cobol - Condition Statements</a></li>
+<li><a href="/cobol/cobol_loop_statements.htm">Cobol - Loop Statements</a></li>
+<li><a href="/cobol/cobol_string_handling.htm">Cobol - String Handling</a></li>
+<li><a href="/cobol/cobol_table_processing.htm">Cobol - Table Processing</a></li>
+<li><a href="/cobol/cobol_file_handling.htm">Cobol - File Handling</a></li>
+<li><a href="/cobol/cobol_file_organization.htm">Cobol - File Organization</a></li>
+<li><a href="/cobol/cobol_file_access_mode.htm">Cobol - File Access Mode</a></li>
+<li><a href="/cobol/cobol_file_handling_verbs.htm">Cobol - File Handling Verbs</a></li>
+<li><a href="/cobol/cobol_subroutines.htm">Cobol - Subroutines</a></li>
+<li><a href="/cobol/cobol_internal_sort.htm">Cobol - Internal Sort</a></li>
+<li><a href="/cobol/cobol_database_interface.htm">Cobol - Database Interface</a></li>
+<li><a href="/cobol/cobol_interview_questions.htm">Cobol - Interview Questions</a></li>
+</ul>
+<ul class="nav nav-list primary left-menu">
+<li class="heading">Cobol Resources</li>
+<li><a href="/cobol/cobol_quick_guide.htm">Cobol - Quick Guide</a></li>
+<li><a href="/cobol/cobol_useful_resources.htm">Cobol - Useful Resources</a></li>
+</ul>
+<?php include ("../selected_reading.php"); ?>
